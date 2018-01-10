5aaa63a448dcaa12a98bed54ddb7d483f126072d
Harsh Patel <32356267+harsh-2711@users.noreply.github.com>
2018-01-10 16:34:41 +0530

    Updated MainActivity
    Here I have added an intent which call Choice.java class when 'multiplayer' button is pressed through which choice for number of players can be asked.

--- app/src/main/java/harshvasoya/gameofcards/MainActivity.java
+++ app/src/main/java/harshvasoya/gameofcards/MainActivity.java
@@ -8,8 +8,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button play;
+    Button play,play1;
 
+    int x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +20,23 @@ public class MainActivity extends AppCompatActivity {
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                x = 1;
                 Intent intent = new Intent(MainActivity.this,Table.class);
+                Bundle b1 = new Bundle();
+                b1.putInt("no_players",x);
+                intent.putExtras(b1);
                 startActivity(intent);
             }
         });
+
+        play1 = (Button) findViewById(R.id.btn_play2);
+        play1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this,Choice.class);
+                startActivity(intent1);
+            }
+        });
     }
+
 }
